8721ca597cb722f7bf41ade0750c27a4318da7b7
JONPIRES <jonbpires@gmail.com>
2023-02-22 18:27:43 -0300

    Add audio files and implement them. have all cards show back up at the end of the game.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const deck = document.querySelectorAll(".card");
+const body = document.querySelector("body");
 
 // Audio effects
-var breathing = new Audio("sounds/vader_breathing.mp3");
-var cardClick = new Audio("sounds/lightsaber.mp3");
+const breathing = new Audio("sounds/vader_breathing.mp3");
+const cardClick = new Audio("sounds/lightsaber.mp3");
+const introSound = new Audio("sounds/theme-song.mp3");
+const yodaLaugh = new Audio("sounds/yoda.wav");
+const vaderMatch = new Audio("sounds/vader_yes.mp3");
 
-breathing.play;
+
+// functions
 
 (function shuffle(){
     deck.forEach(card => {
@@ -12,27 +17,66 @@ breathing.play;
         card.style.order = randomNum;
     })
     })()
+    function spinMatched(){
+        setTimeout(() => {
+            matched[matched.length - 1].style.transform = "rotateY(990deg)";
+            matched[matched.length - 2].style.transform = "rotateY(990deg)";
+          }, "700")
+    }
+    function matchSound(){
+        setTimeout(() => {
+            vaderMatch.volume = 0.1
+            vaderMatch.play();
+          }, "100")
+    }
+    function noMatch(){
+        setTimeout(() => {
+            yodaLaugh.volume = 0.2
+            yodaLaugh.play();
+          }, "100")
+    }
 
+    function returnCards(){
+        setTimeout(() =>{
+            for (let i = 0; i < matched.length; i++) {
+                matched[i].style.transform = "rotateY(180deg)";
+            }
+        }, 2000)
+        matched[15].style.transform = "rotateY(180deg)"
+        matched[14].style.transform = "rotateY(180deg)"
+    }
+
+    function resetGame(){
+
+    }
+
+// Game Logic
 
 let matched = [];
 for (let i = 0; i < deck.length; i++) {
     deck[i].addEventListener('click', function(){
-        console.log(deck[i]);
         if(!matched.includes(deck[i])){
             deck[i].style.transform = "rotateY(180deg)";
             matched.push(deck[i])
             if(matched.length % 2 === 0){
                 if(matched[matched.length - 1].innerHTML === matched[matched.length - 2].innerHTML){
                     console.log("Match!")
+                       matchSound()
+                      spinMatched()
+                      if(matched.length === 16){
+                        introSound.play();
+                        returnCards()
+                      }
                     }
                     else{
                        console.log("No match");
                     setTimeout(() => {
+                        noMatch();
                         matched[matched.length - 1].style.transform = "rotateY(0deg)";
                         matched[matched.length - 2].style.transform = "rotateY(0deg)";
                         matched.pop()
                         matched.pop()
-                      }, "1000")
+                      }, "700")
                     }
     
             }
