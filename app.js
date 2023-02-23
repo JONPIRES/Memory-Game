4b04fb10f90b8bf3538306167402feec9f03d652
JONPIRES <jonbpires@gmail.com>
2023-02-23 19:54:26 -0300

    add reset function and add sound when game starts

--- app.js
+++ app.js
@@ -1,58 +1,92 @@
 const deck = document.querySelectorAll(".card");
 const body = document.querySelector("body");
+const resetBtn = document.querySelector(".reset");
+const soundBtn = document.querySelector(".sound");
 
-// Audio effects
+// Audio files
 const breathing = new Audio("sounds/vader_breathing.mp3");
 const cardClick = new Audio("sounds/lightsaber.mp3");
 const introSound = new Audio("sounds/theme-song.mp3");
 const yodaLaugh = new Audio("sounds/yoda.wav");
 const vaderMatch = new Audio("sounds/vader_yes.mp3");
+const allSound = [cardClick,introSound, yodaLaugh,vaderMatch];
+
+
+
+
 
 
 // functions
 
-(function shuffle(){
+function shuffle(){
     deck.forEach(card => {
         let randomNum = Math.floor(Math.random() *16);
         card.style.order = randomNum;
     })
-    })()
+    }
+    shuffle();
+    
     function spinMatched(){
         setTimeout(() => {
             matched[matched.length - 1].style.transform = "rotateY(990deg)";
             matched[matched.length - 2].style.transform = "rotateY(990deg)";
-          }, "700")
-    }
-    function matchSound(){
-        setTimeout(() => {
-            vaderMatch.volume = 0.1
-            vaderMatch.play();
-          }, "100")
-    }
-    function noMatch(){
-        setTimeout(() => {
-            yodaLaugh.volume = 0.2
-            yodaLaugh.play();
-          }, "100")
+          }, "200")
     }
 
-    function returnCards(){
+    function matchSound(){
+            vaderMatch.volume = 0.2
+            vaderMatch.play();
+    }
+
+    function noMatchSound(){
+            yodaLaugh.volume = 0.25
+            yodaLaugh.play();
+    }
+
+    function endGame(){
+            introSound.volume = 0.1
+            introSound.play();
+    }
+
+    function resetBoard(){
         setTimeout(() =>{
             for (let i = 0; i < matched.length; i++) {
                 matched[i].style.transform = "rotateY(180deg)";
             }
-        }, 2000)
+        }, 1000);
+        setTimeout(()=>{
+            for (let i = 0; i < deck.length; i++) {
+                deck[i].style.transform = "rotateY(0)";
+                shuffle()
+            matched = []
+            }
+        }, 3000)
+       
     }
+    // function resetBoard(){
+      
 
-    function resetGame(){
         
-    }
+    // }
+
+   
+    // soundBtn.addEventListener("click", function (){
+    //     allSound.forEach(sound, () =>{
+    //         sound.muted = true
+    //         sound.pause();
+    //         console.log("working")
+    //     })
+    // });
+
 
 // Game Logic
 
 let matched = [];
 for (let i = 0; i < deck.length; i++) {
     deck[i].addEventListener('click', function(){
+        // breathing.play()
+        introSound.play()
+        introSound.volume = 0.03;
         if(!matched.includes(deck[i])){
             deck[i].style.transform = "rotateY(180deg)";
             matched.push(deck[i])
@@ -62,16 +96,16 @@ for (let i = 0; i < deck.length; i++) {
                        matchSound()
                       spinMatched()
                       if(matched.length === 16){
-                        introSound.play();
-                        returnCards()
+                        // endGame()
+                        resetBoard()
                       }
                     }
                     else{
                        console.log("No match");
                     setTimeout(() => {
-                        noMatch();
-                        matched[matched.length - 1].style.transform = "rotateY(0deg)";
-                        matched[matched.length - 2].style.transform = "rotateY(0deg)";
+                        noMatchSound();
+                        matched[matched.length - 1].style.transform = "rotateY(0)";
+                        matched[matched.length - 2].style.transform = "rotateY(0)";
                         matched.pop()
                         matched.pop()
                       }, "700")
