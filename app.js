784f825cc97226dfccef6e109de0bff8b013c84e
JONPIRES <jonbpires@gmail.com>
2023-02-24 12:26:29 -0300

    sound button working and attempts feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const deck = document.querySelectorAll(".card");
 const body = document.querySelector("body");
+const attemptsMsg = document.querySelector(".attempts");
 const resetBtn = document.querySelector(".reset");
 const soundBtn = document.querySelector(".sound");
 
@@ -11,12 +12,17 @@ const yodaLaugh = new Audio("sounds/yoda.wav");
 const vaderMatch = new Audio("sounds/vader_yes.mp3");
 const allSound = [cardClick,introSound, yodaLaugh,vaderMatch];
 
-
-
+let attempts = 0;
+let matched = [];
 
 
 
 // functions
+
+function updateAttempt(){
+    attempts += 1
+    attemptsMsg.innerHTML = `${attempts} Attempts`
+}
 
 function shuffle(){
     deck.forEach(card => {
@@ -27,6 +33,7 @@ function shuffle(){
     shuffle();
     
     function spinMatched(){
+        attemptsMsg.innerHTML = `${attemptNumber} Attempts`
         setTimeout(() => {
             matched[matched.length - 1].style.transform = "rotateY(990deg)";
             matched[matched.length - 2].style.transform = "rotateY(990deg)";
@@ -34,17 +41,19 @@ function shuffle(){
     }
 
     function matchSound(){
-            vaderMatch.volume = 0.2
             vaderMatch.play();
+        vaderMatch.volume = 0.05
+
     }
 
     function noMatchSound(){
-            yodaLaugh.volume = 0.05
             yodaLaugh.play();
+        yodaLaugh.volume = 0.05
+
     }
 
-    function endGame(){
-            introSound.volume = 0.1
+    function introMusic(){
+            introSound.volume = 0.03
             introSound.play();
     }
 
@@ -59,54 +68,97 @@ function shuffle(){
                 deck[i].style.transform = "rotateY(0)";
                 shuffle()
             matched = []
+            attempts = []
             }
         }, 3000)
        
     }
+
+    // buttons functions
  
     soundBtn.addEventListener("click", function (){
        if(soundBtn.value === "ON"){
-        console.log("btn is on")
+        soundBtn.value = "OFF"
+        console.log("sound OFF")
         soundBtn.innerHTML = "Sound OFF"
         allSound.forEach(function(sound){
             sound.volume = 0
         })
        }
-    });
-
-
-    soundBtn.addEventListener("click", function (){
-       if(soundBtn.value === "ON"){
-        console.log("btn is on")
+       else if(soundBtn.value === "OFF"){
+        soundBtn.value = "ON";
+        console.log("Sound on")
+        soundBtn.innerHTML = "Sound ON"
+        introSound.volume = 0.03;
+        yodaLaugh.volume = 0.05
+        vaderMatch.volume = 0.2
        }
     });
 
+    // Reset buton \/
+
+    // resetBtn.addEventListener("click", function (){
+    //    if(resetBtn.value === "ON"){
+    //     console.log("btn is on")
+    //    }
+    // });
+
+// Timer function
+
+let seconds = 0
+let minutes = 0
+let hours = 0
+
+function timer(){
+    setInterval(()=>{
+        if(seconds >= 0 && seconds < 60){
+            seconds += 1;
+        }
+        else if (seconds === 60 && minutes <60){
+        seconds = 0
+        minutes += 1
+        }
+        else if(minutes === 60 && hours < 24){
+            seconds = 0
+            minutes = 0
+            hours += 1
+        }
+        
+        },1000)
+   
+}
+// timer();
 
 // Game Logic
 
-let matched = [];
+
 for (let i = 0; i < deck.length; i++) {
     deck[i].addEventListener('click', function(){
-        // breathing.play()
-        introSound.play()
-        introSound.volume = 0.03;
+        if(soundBtn.value === "ON"){
+            introMusic()
+            }
         if(!matched.includes(deck[i])){
             deck[i].style.transform = "rotateY(180deg)";
             matched.push(deck[i])
             if(matched.length % 2 === 0){
                 if(matched[matched.length - 1].innerHTML === matched[matched.length - 2].innerHTML){
                     console.log("Match!")
-                       matchSound()
-                      spinMatched()
+                    spinMatched()
+                    updateAttempt()
+                     if(soundBtn.value === "ON"){
+                         matchSound()
+                     }
                       if(matched.length === 16){
-                        // endGame()
                         resetBoard()
                       }
                     }
                     else{
-                       console.log("No match");
+                        updateAttempt()
+                    console.log("No match");
+                    if(soundBtn.value === "ON"){
+                     noMatchSound();
+                     }
                     setTimeout(() => {
-                        noMatchSound();
                         matched[matched.length - 1].style.transform = "rotateY(0)";
                         matched[matched.length - 2].style.transform = "rotateY(0)";
                         matched.pop()
